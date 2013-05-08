1af16d36e630cb2808622f165689c96c56ae9bd7
mitkof6 <jimstanev@gmail.com>
2013-05-08 10:34:14 +0300

    Working version. +Book +Check in +Check out +Free rooms -Payment -Make offers

---
+++ src/database/DatabaseGetSelect.java
@@ -0,0 +1,58 @@
+package database;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import javax.swing.JTextArea;
+
+import com.mysql.jdbc.PreparedStatement;
+
+public class DatabaseGetSelect extends Database{
+
+	ResultSet rs = null;
+	
+	public DatabaseGetSelect(JTextArea textArea, String command){
+		super(command);
+		this.textArea = textArea;
+		//execute(command);
+	}
+	
+	@Override
+	public boolean execute() {
+		
+		PreparedStatement ps = null;
+		
+		try {
+			ps = (PreparedStatement) connection.prepareStatement(command);
+			rs = ps.executeQuery();
+			return true;
+		} catch (SQLException e) {
+			textArea.append(e.getErrorCode()+"\n"+e.getMessage()+
+							"\n"+e.getSQLState()+"\n");
+			return false;
+		}
+	}
+	
+	public ResultSet getResult(){
+		if(connectionState==true&&execute()){
+			return this.rs;
+		}else{
+			return null;
+		}
+	}
+	
+	public void closeAll(){
+		if(rs!=null){
+			try {
+				rs.close();
+				
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+		closeConnection();
+		
+	}
+
+}
